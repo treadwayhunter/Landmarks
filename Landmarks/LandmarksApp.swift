c33ad8dc880848a30334cd6a2841f0248cc590d5
Hunter Treadway <treadwayhunter@gmail.com>
2024-05-16 16:21:02 -0500

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Hunter Treadway on 5/16/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
